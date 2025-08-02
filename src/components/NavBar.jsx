c661e3b4c816c855bb2302f8fc73160b116fd4e1
yaser-alhamad <yaser.alhamad2016@gmail.com>
2025-08-02 16:29:17 +0300

    first commit

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NavBar = () => {
+  return <div className="bg-red-800">NavBar</div>;
+};
+
+export default NavBar;
---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import HomePage from "./pages/HomePage";
+// import App from "./App.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
